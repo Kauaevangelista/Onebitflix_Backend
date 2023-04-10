d9edfe93a72914c22de55d16cecf41f24c6444de
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-10 12:00:09 -0300

    Configurando o AdminJS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import { database } from './database'
+import { adminJs, adminJsRouter } from './adminjs'
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+app.use(express.static('public'))
 
-app.listen(PORT, () => {
-  console.log(`Server started successfuly at port ${PORT}`)
+app.use(adminJs.options.rootPath, adminJsRouter)
+
+const PORT = process.env.port || 3000
+
+app.listen(PORT, async () => {
+  await database.authenticate().then(() => {
+    console.log('DB connection successfull.')
+  })
+
+  console.log(`Server started successfuly at port ${PORT}.`)
 })
