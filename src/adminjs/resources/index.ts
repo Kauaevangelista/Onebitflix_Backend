07422bd39908f16fee0b61dafeeff051680adff4
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-13 12:22:51 -0300

    Tabela de episódios

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,12 +1,17 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
+import { episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions
   },
   {
     resource: Category,
