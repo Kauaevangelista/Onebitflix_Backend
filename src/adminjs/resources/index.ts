e7f7ba4b30215c711ad541b52193220538a4a01d
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-14 08:59:52 -0300

    Upload de Thumbnails

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,13 +1,14 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
-import { courseResourceOptions } from "./course";
+import { courseResourceOptions, courseResourceFeatures } from "./course";
 import { episodeResourceOptions, episodeResourceFeatures } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
-    options: courseResourceOptions
+    options: courseResourceOptions,
+    features: courseResourceFeatures
   },
   {
     resource: Category,
