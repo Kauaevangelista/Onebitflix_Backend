614cbe4552d9121138e5a491909a71046f7fa689
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-20 12:38:45 -0300

    Buscando por cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { courseService } from '../services/courseService'
+import { getPaginationParams } from '../helpers/getPaginationParams'
 
 export const coursesController = {
   // GET /courses/:id
@@ -37,5 +38,20 @@ export const coursesController = {
           return res.status(400).json({ message: err.message })
         }
       }
-    }
-}
+    },
+
+     // GET /courses/search?name=
+     search: async (req: Request, res: Response) => {
+      const { name } = req.query
+      const [page, perPage] = getPaginationParams(req.query)
+  
+      try {
+        if (typeof name !== 'string') throw new Error('name param must be of type string');
+        const courses = await courseService.findByName(name, page, perPage)
+        return res.json(courses)
+      } catch (err) {
+        if (err instanceof Error) {
+          return res.status(400).json({ message: err.message })
+        }
+      }
+    }}
