6e5cdf16ae2d32edf0bcf93bfce801e820f28e5c
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-25 12:28:30 -0300

    Obtendo os cursos favoritos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,6 +1,28 @@
 import { Favorite } from "../models/Favorite"
 
 export const favoriteService = {
+
+  findByUserId: async (userId: number) => {
+    const favorites = await Favorite.findAll({
+      attributes: [['user_id', 'userId']],
+      where: { userId },
+      include: {
+        association: 'Course',
+        attributes: [
+          'id',
+          'name',
+          'synopsis',
+          ['thumbnail_url', 'thumbnailUrl']
+        ]
+      }
+    })
+
+    return {
+      userId,
+      courses: favorites.map(favorite => favorite.Course)
+    }
+  },
+
 	create: async (userId: number, courseId: number) => {
     const favorite = await Favorite.create({
       userId,
