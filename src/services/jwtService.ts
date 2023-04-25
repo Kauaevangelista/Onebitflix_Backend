a2754ea1ab9519d36d067d283e6ed0b5a47fb2e5
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-25 09:48:10 -0300

    Protegendo os vídeos

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -7,5 +7,9 @@ const secret = 'chave-do-jwt'
 export const jwtService = {
   signToken: (payload: string | object | Buffer, expiration: string) => {
     return jwt.sign(payload, secret, { expiresIn: expiration })
+  },
+
+  verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn)
   }
 }
